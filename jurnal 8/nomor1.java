8dd7c91ddb7c86a7754a95247d764bd95cf3680b
muthia6706154158 <muthiavirliani08@gmail.com>
2017-04-07 09:59:55 +0700

    Update nomor1.java

--- jurnal 8/nomor1.java
+++ jurnal 8/nomor1.java
@@ -4,14 +4,14 @@ public class nomor1 {
 	public static void main(String[]args){
 		Scanner show = new Scanner(System.in);
 		System.out.print("Masukkan jumlah : ");
-		int n =show.nextInt();
+		int bil =show.nextInt();
 		long fib[] = new long[];
 		fib[0]=0;
 		fib[1]=1;
-	for(int i =2 ; i<n;i++){
+	for(int i =2 ; i<bil;i++){
 		fib [i] = fib [i-1] + fib[i-2];
 	}
-	for(int i =0 ; i<n;i++){
+	for(int i =0 ; i<bil;i++){
 		System.out.print(fib[i]+" ");
 	}
 	}
